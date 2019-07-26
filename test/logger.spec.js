466218dc8b94be3ed38ccf21abc47cb67c3cd1f0
Charlie McClung <charlie@bowtie.co>
2019-07-26 10:51:52 -0600

    Update test coverage and add coverage reporting

--- test/logger.spec.js
+++ test/logger.spec.js
@@ -66,6 +66,59 @@ describe('Logger', function () {
     })
   })
 
+  it('should throw error attempting to override existing method', function () {
+    const testLogger = new Logger({
+      allowForceNoThrow: true,
+      verbose: true
+    })
+
+    const badConfig = Object.assign({}, testConfig, {
+      getTimeStamp: {
+        stamp: true,
+        prefix: 'BAD',
+        color: 'red'
+      }
+    })
+
+    const attempt = () => {
+      testLogger.enableLogging(badConfig)
+    }
+
+    expect(attempt).to.throw('Cannot override method for logging: \'getTimeStamp\' ... it already exists!')
+  })
+
+  it('should colorize basic string', function () {
+    const stringInput = 'Hello world'
+    const colorOutput = Logger.colorize(stringInput, 'red')
+
+    expect(colorOutput).to.eql(`\u001b[31m${stringInput}\u001b[39m`)
+  })
+
+  it('should return unaltered string with invalid color', function () {
+    const stringInput = 'Hello world'
+    const colorOutput = Logger.colorize(stringInput, 'bscolor')
+
+    expect(colorOutput).to.eql(stringInput)
+  })
+
+  it('should return unaltered number with number args', function () {
+    const numberInput = 10
+    const colorOutput = Logger.colorize(numberInput, 'red')
+
+    expect(colorOutput).to.eql(numberInput)
+  })
+
+  it('should colorize a non-array object', function () {
+    const objectInput = {
+      hello: 'world',
+      foo: 'bar'
+    }
+
+    const colorOutput = Logger.colorize(objectInput, 'red')
+
+    expect(colorOutput).to.eql(objectInput)
+  })
+
   afterEach(function () {
     this.sinon.restore()
   })
